eedc29368b0356f256119fc763ba7d30cad8c41c
Victor <Vmalho393@west-mec.edu>
2022-01-02 18:30:38 -0800

    final changes

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light border-top">
-          <div class="container m-auto">
-            <a href="#" class="m-auto" style="text-decoration: none;">Developed by Dev Ninja</a>
-          </div>
-        </nav>
+    <div class="container m-auto">
+        <a href="http://victormalhotra.ml" class="m-auto" style="text-decoration: none;">Developed by Victor
+            Malhotra</a>
+    </div>
+</nav>
